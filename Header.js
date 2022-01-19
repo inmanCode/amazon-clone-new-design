8406f21faa87269055052653dbcf02fdbeb0d218
Aymane Abssi <aymane.abssi@dotenv.it>
2022-01-19 11:43:14 +0100

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,14 +1,21 @@
-const getcartItems = () => {
-  db.collection('cart-items').onSnapshot((Snapshot) => {
-    let totalCount = 0;
-    Snapshot.forEach((doc) => {
-      totalCount += doc.data().quantity;
-    });
-    setCartCounter(totalCount);
-  });
-};
+// const getcartItems = () => {
+//   db.collection('cart-items').onSnapshot((Snapshot) => {
+//     let totalCount = 0;
+//     Snapshot.forEach((doc) => {
+//       totalCount += doc.data().quantity;
+//     });
+//     setCartCounter(totalCount);
+//   });
+// };
 
-const setCartCounter = (totalCount) => {
-  document.querySelector('.cart_number').innerText = totalCount;
-};
-getcartItems();
+// const setCartCounter = (totalCount) => {
+//   document.querySelector('.cart_number').innerText = totalCount;
+// };
+// getcartItems();
+const name = 'Rachel';
+
+const age = 31;
+
+const person = { name, age };
+
+console.log(person);
